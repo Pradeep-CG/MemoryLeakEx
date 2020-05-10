d51709fe6651c4ffd0680874ad36c6bf8f8d0a5c
iOS-Pradeep <pradeep.sagar.ios@gmail.com>
2020-05-10 20:45:46 +0530

    first commit

--- MemoryLeakEx/ViewController.swift
+++ MemoryLeakEx/ViewController.swift
@@ -13,8 +13,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next Page", style: .plain, target: self, action: #selector(goToNextPage))
     }
 
-
+    @objc func goToNextPage(){
+        
+        navigationController?.pushViewController(SecondViewController(), animated: true)
+    }
 }
 
+class Service {
+    
+    var secondVC: SecondViewController?
+    //weak var secondVC: SecondViewController? // use this to resolve the memory leak
+}
+
+class SecondViewController: UITableViewController{
+    
+    let service = Service()
+    
+    deinit {
+        print("os reclaming memory for secondviewcontroller")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        service.secondVC = self
+        
+        view.backgroundColor = .red
+    }
+}
